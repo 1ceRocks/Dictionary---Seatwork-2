259fb6f8fe1f2d0c917fbaa90f1375974d80081b
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-11-12 13:02:06 +0800

    Assignment Instructions labeled as Comment.

---
+++ dictionary-list.py
@@ -0,0 +1,20 @@
+# Write a python program for contact tracing:
+
+# - Display a menu of options
+# 	Menu:
+# 		 1 -> Add an item
+# 		 2 -> Search
+# 		 3 -> Exit (y/n)
+# - Allow user to select item in the menu (check if valid)
+# - Perform the selected option
+# 		- Option 1: Ask personal data for contact tracing (Listed are sample only, add more)
+# 				   Use dictionary to store the info
+# 				   Use the full name as key
+# 				   The value is another dictionary of personal information
+# 		- Option 2: Search, ask full name then display the record
+# 		- Option 3: Ask the user if want to exit or retry.
+
+# Note: 
+# - Your program should be uploaded to github before 4pm
+# - Record a demo presenting your program
+# - Send the demo or link of demo directly to my messenger
