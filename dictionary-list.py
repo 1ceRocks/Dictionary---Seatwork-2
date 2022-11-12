ca393e40ce692c621b5ea24fdba8e73878994b73
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-11-12 13:41:03 +0800

    Analyzing the right function to be used to create a new key data stored on the personalData main dictionary.

--- dictionary-list.py
+++ dictionary-list.py
@@ -19,19 +19,26 @@
 # - Record a demo presenting your program
 # - Send the demo or link of demo directly to my messenger
 
-#importing library for implementing additional features to the program
+# importing library for implementing additional features to the program
 import os
 
 # first and foremost, let's make a dictionary list for creating a stored value data within user input for back-end programming.
 personalData = {
-    "dataBase" : {} # for search (2 - option) reference
+    "dataBase" : {"Full Name" : "IceRocks", "Age" : 20, "Gender" : "M", "Address" : "Manila, Philippines"} # random stored data for search (2 - option) labeled as reference
 }
 
-#next, of course, we want to make a display menu of options using print()
+# next, of course, we want to make a display menu of options using print()
 os.system("cls")
 print("Your Dictionary List")
 print("Selection Menu")
 print("1 -> Add an item")
 print("2 -> Search")
 print("3 -> Exit (y/n)")
+
+# let's now create the user-input program interface
 os.system("cls")
+usr_Input = int(input("What program menu do you want to do? (1 / 2 / 3)\n\nYour answer here: ")) # this would be the user-input for selecting between 1 , 2 , or 3.
+if usr_Input == 1:
+    print("Selecting No.1 would automatically create a new database for you stored on the main personalData - Contact Tracing")
+    personalData["dataBase1"] = {} # this program did not meet a new key data so this has to be fixed
+    print(personalData)
